1c986f4d9d6a3e09f1c46dc23e2998e39bebcedc
Vyacheslav <VyacheslavZarudenko@gmail.com>
2020-11-19 00:11:17 +0200

    project ome exersise 1

--- project one/js/js.js
+++ project one/js/js.js
@@ -1,1 +1,19 @@
 "use strict";
+
+    const numberOfFilms = +prompt('скільки фільмів ви вже переглянули?', '');
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    for(let i = 0; i < 2; i++){
+        let film = prompt('один із останніх переглянутих фільмів?','');
+        let ocinka = prompt('на скільки ви оцінюєте його','');
+
+        personalMovieDB.movies[film] = ocinka; 
+    }
+
+    console.log(personalMovieDB);
