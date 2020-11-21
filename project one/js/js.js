7d56e6f1ff5cebf78d6d60570004ca6ffc13a79a
Vyacheslav <VyacheslavZarudenko@gmail.com>
2020-11-22 01:34:17 +0200

    2

--- project one/js/js.js
+++ project one/js/js.js
@@ -1,6 +1,10 @@
 "use strict";
 
-    const numberOfFilms = +prompt('скільки фільмів ви вже переглянули?', '');
+let numberOfFilms = '';
+    while(numberOfFilms == null || numberOfFilms == '' || numberOfFilms.length > 50){
+        numberOfFilms = +prompt('скільки фільмів ви вже переглянули?', '');
+    }
+
     const personalMovieDB = {
         count: numberOfFilms,
         movies: {},
@@ -10,10 +14,21 @@
     };
 
     for(let i = 0; i < 2; i++){
-        let film = prompt('один із останніх переглянутих фільмів?','');
-        let ocinka = prompt('на скільки ви оцінюєте його','');
-
+        let film = '',
+            ocinka = '';
+            while(film == null || film ==''|| film.length > 50){
+                film = prompt('один із останніх переглянутих фільмів?','');
+            }
+            while(ocinka == null || ocinka ==''|| ocinka.length >50){
+                ocinka = prompt('на скільки ви оцінюєте його','');
+            }
         personalMovieDB.movies[film] = ocinka; 
     }
 
-    console.log(personalMovieDB);
+ if(personalMovieDB.count < 10){
+     alert('переглянуто мало фільмів');
+ } else if(personalMovieDB.count < 30){
+    alert('ви класичний глядач');
+ } else{
+    alert('ви кіноман');
+ }
