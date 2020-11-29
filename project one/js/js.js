8dd37454940d10a61cdb4ba3a705211ff26b7b32
Vyacheslav <VyacheslavZarudenko@gmail.com>
2020-11-29 17:10:34 +0200

    4

--- project one/js/js.js
+++ project one/js/js.js
@@ -14,53 +14,49 @@ function start(){
         movies: {},
         actors: {},
         genres: [],
-        privat: false
+        privat: false,
+        rememberMyFilms: function(){
+            for(let i = 0; i < 2; i++){
+                let film = '',
+                    ocinka = '';
+                    while(film == null || film ==''|| film.length > 50){
+                        film = prompt('один із останніх переглянутих фільмів?','');
+                    }
+                    while(ocinka == null || ocinka ==''|| ocinka.length >50){
+                        ocinka = prompt('на скільки ви оцінюєте його','');
+                    }
+                personalMovieDB.movies[film] = ocinka; 
+            }
+        },
+        detectPersonalLevel: function(){
+            if(personalMovieDB.count < 10){
+                alert('переглянуто мало фільмів');
+            } else if(personalMovieDB.count < 30){
+               alert('ви класичний глядач');
+            } else{
+               alert('ви кіноман');
+            }
+         },
+        showMyDB: function(privetOrNot){
+            if(!privetOrNot){
+                console.log(personalMovieDB);
+            }
+        },
+        writeYourGenres: function(){
+            for(let i = 0; i < 3; i++){
+                personalMovieDB.genres[i] = prompt(`ваш улуюблений жанр під номером ${i + 1}`, '');
+            }
+        },
+        toggleVisibleMyDB: function(){
+            (personalMovieDB.privat) ? personalMovieDB.privat = false : personalMovieDB.privat = true ;
+        }
     };
 
    
-function rememberMyFilms(){
-    for(let i = 0; i < 2; i++){
-        let film = '',
-            ocinka = '';
-            while(film == null || film ==''|| film.length > 50){
-                film = prompt('один із останніх переглянутих фільмів?','');
-            }
-            while(ocinka == null || ocinka ==''|| ocinka.length >50){
-                ocinka = prompt('на скільки ви оцінюєте його','');
-            }
-        personalMovieDB.movies[film] = ocinka; 
-    }
-}
-
-rememberMyFilms();
 
 
-
- function detectPersonalLevel(){
-    if(personalMovieDB.count < 10){
-        alert('переглянуто мало фільмів');
-    } else if(personalMovieDB.count < 30){
-       alert('ви класичний глядач');
-    } else{
-       alert('ви кіноман');
-    }
- }
-
- detectPersonalLevel();
-
-
- function showMyDB(privetOrNot){
-    if(!privetOrNot){
-        console.log(personalMovieDB);
-    }
- }
-
- showMyDB(personalMovieDB.privat);
-
- function writeYourGenres(){
-     for(let i = 0; i < 3; i++){
-         personalMovieDB.genres[i] = prompt(`ваш улуюблений жанр під номером ${i + 1}`, '');
-     }
- }
-
- writeYourGenres();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
