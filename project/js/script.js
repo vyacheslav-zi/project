9ac2f1835691e5bd7b4526e56d115804848b9415
Vyacheslav <VyacheslavZarudenko@gmail.com>
2020-12-06 20:09:59 +0200

    6

--- project/js/script.js
+++ project/js/script.js
@@ -24,3 +24,54 @@ const movieDB = {
     ]
 };
 
+document.querySelectorAll('.promo__adv img').forEach(item=>item.remove());
+document.querySelector('.promo__genre').textContent = 'драма'; 
+document.querySelector('.promo__bg').style.cssText = 'background: url(img/bg.jpg) center center/cover no-repeat';
+
+
+function sortDB(func){
+    movieDB.movies.sort();
+    func();
+}
+
+function reFull(){
+    const filmList = document.querySelector('.promo__interactive-list');
+    filmList.innerHTML = '';
+    movieDB.movies.forEach((item, number)=>{
+        filmList.innerHTML += `
+        <li class="promo__interactive-item">${number + 1}. ${item}
+        <div class="delete"></div>
+        </li>
+        `;
+    });
+
+    document.querySelectorAll('.delete').forEach((btn, i) =>{
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+        });
+    });
+}
+
+sortDB(reFull);
+
+
+document.querySelector('button').addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    const check = document.querySelector('[type="checkbox"]');
+    if(check.checked){
+        console.log('додаємо улублений фільм');
+    }
+
+    let addMove = document.querySelector('.adding__input').value;
+
+        if(addMove.length > 21){
+            addMove = `${addMove.substring(0, 22)}...`;
+        }
+        
+    movieDB.movies.push(addMove);
+    sortDB(reFull);
+});
+
+
